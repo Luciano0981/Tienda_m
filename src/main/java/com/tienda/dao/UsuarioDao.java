c3d9f942b9a43673cc7c96c2af7ced4f3b45b798
LUIS <LUIS@DESKTOP-MCOPROS.lan>
2023-11-27 22:45:51 -0600

    Semana 12

--- src/main/java/com/tienda/dao/UsuarioDao.java
+++ src/main/java/com/tienda/dao/UsuarioDao.java
@@ -7,8 +7,15 @@ package com.tienda.dao;
 import com.tienda.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+// se hace una busqueda por username
 public interface UsuarioDao extends JpaRepository<Usuario, Long>{
     public Usuario findByUsername(String username);
+    
+    
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
     
 }
